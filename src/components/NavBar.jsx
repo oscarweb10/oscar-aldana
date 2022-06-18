a354fc7abad603c12d0ade22a727d86cb732c20d
oscaraldana <oscaraldana10@gmail.com>
2022-06-18 13:30:48 -0400

    desafio 2 barra nabvar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+      <div>
+      <h1>Eléctrico <span>EN LINEA</span></h1>
+    <ul>
+         <li><a class="active" href="#home">Inicio</a></li>
+        <li><a href="#empresa">Empresa</a></li>
+        <li><a href="#productos">Productos</a></li>
+        <li><a href="#galeria">Galeria</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+    </ul>
+
+     </div>
+     )
+}
+
+export default NavBar
